cb5bedea06c7f81211affa33b1bc602907733b31
can97 <can97@DESKTOP-ETFFE43>
2020-11-28 21:41:44 +0000

    Kata2Version4

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -8,7 +8,7 @@ public class Kata2 {
 
 
     public static void main(String[] args) {
-        int [] data = {-2,2,-2,3,-4,-9,-2,3,2,2,3,5,2,1,1};
+        Integer [] data = {-2,2,-2,3,-4,-9,-2,3,2,2,3,5,2,1,1};
         Histogram histo = new Histogram(data);
         Map<Integer, Integer> histogr = histo.getHistogram();
         for (HashMap.Entry entry : histogr.entrySet()) {
