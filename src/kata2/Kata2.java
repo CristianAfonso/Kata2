6d55afd9adff9d8c4921a656df4e120c64350500
can97 <can97@DESKTOP-ETFFE43>
2020-11-28 21:14:17 +0000

    Kata2Version2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -9,13 +9,8 @@ public class Kata2 {
     public static void main(String[] args) {
         int [] data = {-2,2,-2,3,-4,-9,-2,3,2,2,3,5,2,1,1};
         HashMap<Integer, Integer> histogram = new HashMap<Integer, Integer>();
-        for (int i = 0; i < data.length; i++) {
-            
-            if (histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }else{ 
-                histogram.put(data[i], 1);
-            }
+        for (int i: data) {
+            histogram.put(i, histogram.containsKey(i) ? histogram.get(i) + 1 : 1);
         }
         Iterator ent = histogram.entrySet().iterator();
         while (ent.hasNext()){
